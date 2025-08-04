0a66215425e8ca1da9d41fa1b5199a5ab75d1c4a
Gromp-13 <andrei142005@gmail.com>
2025-08-04 15:37:11 +0700

    04.08.25 1

---
+++ internal/db/repository/subscriptions.go
@@ -0,0 +1,40 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/Gromp-13/IronGym/internal/models"
+)
+
+func (repo *PGRepo) NewSubscription(sub models.Subscriptions) error {
+	_, err := repo.pool.Exec(context.Background(), `
+	INSERT INTO subscriptions (clientid, startdate, durationdays, enddate)
+	VALUESc($1, $2, $3, $4)`,
+		sub.ClientID,
+		sub.StartDate,
+		sub.DurationDays,
+		sub.EndDate,
+	)
+	return err
+}
+
+func (repo *PGRepo) GetLastClientByBarcode(barcode string) (models.Client, error) {
+	var c models.Client
+	err := repo.pool.QueryRow(context.Background(), `
+	SELECT id, lastname, firstname, middlename, phonenumber, birthdate, cardbarcode, gender
+	FROM clients
+	WHERE cardbarcode = $1
+	ORDER BY id DESC
+	LIMIT 1
+	`, barcode).Scan(
+		&c.ID,
+		&c.LastName,
+		&c.FirstName,
+		&c.MiddleName,
+		&c.PhoneNumber,
+		&c.BirthDate,
+		&c.CardBarcode,
+		&c.GenderID,
+	)
+	return c, err
+}
--- internal/ui/screens/newclientscreens.go
+++ internal/ui/screens/newclientscreens.go
@@ -2,14 +2,41 @@ package screens
 
 import (
 	"fmt"
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
+	"github.com/Gromp-13/IronGym/internal/db"
+	"github.com/Gromp-13/IronGym/internal/models"
 )
 
+type SubscriptionOption struct {
+	Label    string
+	Duration int
+}
+
 func NewClientScreen(a fyne.App) {
+
+	var subscriptionOption = []SubscriptionOption{
+		{"Мужской", 30},
+		{"Женский", 30},
+		{"Школьный", 30},
+		{"Студенчиский", 30},
+		{"Мужской 3 месяца", 90},
+		{"Мужской пол года", 1800},
+		{"Мужской на год", 365},
+		{"Женский 3 месяца", 90},
+		{"Женский пол года", 1800},
+		{"Женский на год", 365},
+	}
+	var selectedSubscription SubscriptionOption
+
+	options := make([]string, len(subscriptionOption))
+	for i, o := range subscriptionOption{
+		options[i] = o.Label
+	}
 
 	windowNCS := a.NewWindow("Новый клиент")
 	windowNCS.Resize(fyne.NewSize(400, 500))
@@ -44,29 +71,75 @@ func NewClientScreen(a fyne.App) {
 	birthdate.Move(fyne.NewPos(50, 290))
 	birthdate.SetPlaceHolder("Штрихкод карты")
 
-	selectpas := widget.NewSelect(
-		[]string{
-			"Мужской",
-			"Женский",
-			"Студенчиски",
-			"Школьный",
-			"Мужской 3 месяца",
-			"Мужской пол года",
-			"Мужской на год",
-			"Женский 3 месяца",
-			"Женский пол года",
-			"Женский на год",
-		},
-
+	selectSub := widget.NewSelect(options,
 		func(s string) {
-			fmt.Printf("Select is %s", s)
-		},
-	)
-	selectpas.Resize(fyne.NewSize(300, 40))
-	selectpas.Move(fyne.NewPos(50, 350))
-	selectpas.PlaceHolder = "Выберити абонемент"
+			for _, o := range subscriptionOption{
+				if o.Label == s {
+					selectedSubscription = o
+					break
+				}
+			}
+		})
+	selectSub.Resize(fyne.NewSize(300, 40))
+	selectSub.Move(fyne.NewPos(50, 350))
+	selectSub.PlaceHolder = "Выберити абонемент"
 
-	save := widget.NewButton("Сохранить", func() { fmt.Println("hi") })
+	save := widget.NewButton("Сохранить", 
+	func() {
+		if userlname.Text == "" || userfname.Text == ""{
+			fmt.Println("Фамилия и имя обязательны")
+			return
+		}
+
+		parsedDate, err := time.Parse("2006-01-02", birthdate.Text)
+		if err != nil {
+			fmt.Println("Используйте ГГГГ-ММ-ДД")
+			return
+		}
+
+		if selectedSubscription.Label == ""{
+			fmt.Println("Выберите абонемент")
+			return
+		}
+		client := models.Client{
+			LastName: userlname.Text,
+			FirstName: userfname.Text,
+			MiddleName: patronymic.Text,
+			PhoneNumber: phone.Text,
+			BirthDate: parsedDate,
+			CardBarcode: birthdate.Text,
+			GenderID: 0,
+		}
+
+		err = db.Repo.NewClients(client)
+		if err != nil{
+			fmt.Println("Ошибка при добавлении клиента", err)
+			return
+		}
+
+		createdClients, err, := db.Repo.GetLastClientByBarcode(client.CardBarcode)
+		if err != nil{
+			fmt.Println("Ошибка при поиске клиента:", err)
+			return
+		}
+
+		now := time.Now()
+		sub := models.Subscriptions{
+			ClientID: createdClients.ID,
+			StartDate: now,
+			DurationDays: selectedSubscription.Duration,
+			EndDate: now.AddDate(0, 0, selectedSubscription.Duration),
+		}
+
+		err = db.Repo.NewSubscription(sub)
+		if err != nil{
+			fmt.Println("Ошибка при создании абонимента:", err)
+			return
+		}
+
+		fmt.Println("Клиент и абонимент успешно добавленны")
+		windowNCS.Close()
+	})
 	save.Resize(fyne.NewSize(200, 40))
 	save.Move(fyne.NewPos(100, 410))
 
